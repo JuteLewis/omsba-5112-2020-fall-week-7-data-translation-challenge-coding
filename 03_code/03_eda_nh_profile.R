ccc0a11861b9dbed44c3c1d995f6484dc9edba90
Glen Lewis <glenlewis05@gmail.com>
2020-11-19 10:13:08 -0800

    This commit adds comment changes not captured in last commit.

--- 03_code/03_eda_nh_profile.R
+++ 03_code/03_eda_nh_profile.R
@@ -4,8 +4,10 @@ summary(nh_profile_base)
 # Initial model examining profit against household average age, average years of ----
 # education, and number of male and female household members.
 
-# Variable selection ----
+# Variable selection and model build ----
 # Using Backward Variable Selection
+
+# Following model includes all variables
 bkwd_base <- lm(profit ~ factor(district) + factor(s2aq2) +
                 av_hh_age + male + female + male_help + female_help, 
                 data = nh_profile_base)
@@ -22,16 +24,18 @@ ggplot(bkwd_base, aes(x = fitted(bkwd_base), y = rstandard(bkwd_base))) +
   labs(x = "Profit", y = "Standardized Residuals", 
        title = "Constant Varaince Review for Bkwd Base") 
 
+# Following code removes the av_hh_age variable in refining the model
 bkwd_1 <- lm(profit ~ factor(district) + factor(s2aq2) +
             male + female + male_help + male_help + female_help, 
             data = nh_profile_base)
 summary(bkwd_1)
 
+# Following code expands the bkwd_1 model by exploring the affects of quadratic
 bkwd_1_a <- lm(profit ~ factor(district) + factor(s2aq2) + av_hh_age +
                male + female + male_help + I(male_help^2) + female_help, 
              data = nh_profile_base)
 summary(bkwd_1_a)
-
+# Model fit tests 
 ggplot(bkwd_1_a, aes(x=rstandard(bkwd_1_a))) +
   geom_histogram(binwidth = .10) +
   labs(x = "Standardized Residuals", y = "Residual Count", 
@@ -43,14 +47,12 @@ ggplot(bkwd_1_a, aes(x = fitted(bkwd_1_a), y = rstandard(bkwd_1_a))) +
   labs(x = "Profit", y = "Standardized Residuals", 
        title = "Constant Varaince Review Bkwd Base 1a") 
 
-# Add the quadratic for male help greatly improved the fit of the model.  WIll try 
-# adding a female help quadratic. 
-
+# The following model expands the quadratic exploration. 
 bkwd_1_b <- lm(profit ~ factor(district) + factor(s2aq2) +
                  male + female + male_help + I(male_help^2) + female_help + 
                  I(female_help^2), data = nh_profile_base)
 summary(bkwd_1_b)
-
+# Model fit test
 ggplot(bkwd_1_b, aes(x=rstandard(bkwd_1_b))) +
   geom_histogram(binwidth = .10) +
   labs(x = "Standardized Residuals", y = "Residual Count", 
@@ -62,9 +64,8 @@ ggplot(bkwd_1_b, aes(x = fitted(bkwd_1_b), y = rstandard(bkwd_1_b))) +
   labs(x = "Profit", y = "Standardized Residuals", 
        title = "Constant Varaince Review Bkwd Base 1 b") 
 
-# bkwd_2 alternating district factor and education factor assessing if any new 
+# bkwd_2 alternates the district factor and education factor assessing if any new 
 # levels take on statistical significance  
-
 bkwd_2_a <- lm(profit ~ factor(s2aq2) + male + female + male_help +
                female_help, data = nh_profile_base)
 summary(bkwd_2_a)
@@ -95,8 +96,8 @@ ggplot(bkwd_2_b, aes(x = fitted(bkwd_2_b), y = rstandard(bkwd_2_b))) +
   labs(x = "Proft", y = "Standardized Residuals", 
        title = "Constant Varaince Review") 
 
-# bkwd_3 removing female help variable
-
+# The following model - bkwd_3 - retains the district factor but 
+# removes the female help variable
 bkwd_3 <- lm(profit ~ factor(district)+ male + female + male_help, data = nh_profile_base)
 summary(bkwd_3)
 
@@ -104,8 +105,6 @@ ggplot(bkwd_3, aes(x=rstandard(bkwd_3))) +
   geom_histogram(binwidth = .5) +
   labs(x = "Standardized Residuals", y = "Residual Count", 
        title = "Assumption 6 (Normality) Review") 
-# ASSESSMENT - Error still has a right tail but is shorter than bkwd_1 and 2 but
-# also indicates significant outliers.
 
 ggplot(bkwd_3, aes(x = fitted(bkwd_3), y = rstandard(bkwd_3))) + 
   geom_point() + 
@@ -113,7 +112,7 @@ ggplot(bkwd_3, aes(x = fitted(bkwd_3), y = rstandard(bkwd_3))) +
   labs(x = "Profit", y = "Standardized Residuals", 
        title = "Constant Varaince Review") 
 
-# bkwd_4 removing household female
+# The following code - bkwd_4 - removes the household female variable.
 bkwd_4 <- lm(profit ~ factor(district)+ male + male_help, data = nh_profile_base)
 summary(bkwd_4)
 
